3577790dd1fc2606da66dbda22969fae13699d3b
Lucas Silva <lucas_segundinho@hotmail.com>
2023-05-19 22:32:24 +0100

    Backed project created

---
+++ backend/src/main/java/com/lucaslearning/dslearnbds/DslearnbdsApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucaslearning/dslearnbds/DslearnbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.dslearnbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
