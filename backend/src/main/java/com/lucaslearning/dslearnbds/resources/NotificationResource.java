e634b3c09f4382dfb9fdc1a2b6e77aee56f63078
Lucas Silva <lucas_segundinho@hotmail.com>
2023-06-17 18:33:11 +0100

    Custom content for current user, JPQL query (Notification pt II)

--- backend/src/main/java/com/lucaslearning/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/lucaslearning/dslearnbds/resources/NotificationResource.java
@@ -21,10 +21,11 @@ public class NotificationResource {
 	
 	@GetMapping
 	public ResponseEntity<Page<NotificationDTO>> findAll(
+			@RequestParam(value = "unreadOnly", defaultValue = "false") Boolean unreadOnly,
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "size", defaultValue = "12") Integer size) {
 		PageRequest pageRequest = PageRequest.of(page, size);
-		Page<NotificationDTO> list = service.notificationsForCurrentUser(pageRequest);
+		Page<NotificationDTO> list = service.notificationsForCurrentUser(unreadOnly, pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
 }
